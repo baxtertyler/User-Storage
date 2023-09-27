6f90db28f85c5d93a8ddfe2700d3380ff3450805
Tyler Baxter <tylerbaxter@Tylers-MacBook-Air-49.local>
2023-09-27 15:11:46 -0700

    react app II commit

--- react-frontend/src/Table.js
+++ react-frontend/src/Table.js
@@ -12,9 +12,15 @@ function TableHeader() {
 function TableBody(props) {
     const rows = props.characterData.map((row, index) => {
         return (
-            <tr key={index} >
+            <tr key={index}>
                 <td>{row.name}</td>
                 <td>{row.job}</td>
+                <td>
+                        <button onClick={() => 
+                            props.removeCharacter(index)}>
+                            Delete
+                        </button>
+                </td>
             </tr>
         );
     });
@@ -29,7 +35,8 @@ function Table(props) {
     return (
         <table>
             <TableHeader />
-            <TableBody characterData={props.characterData} />
+            <TableBody characterData={props.characterData} 
+                removeCharacter={props.removeCharacter} />
         </table>
     )
 }
