d5ceec751e649112b49de2d62489539886a8b9fb
Tyler Baxter <tylerbaxter@Tylers-MacBook-Air-49.local>
2023-09-27 15:48:52 -0700

    fina;

--- react-frontend/src/Form.js
+++ react-frontend/src/Form.js
@@ -1,12 +1,14 @@
 import React, {useState} from 'react';
 
-function Form() {
+function Form(props) {
+
     const [person, setPerson] = useState(
         {
             name: "",
             job: "",
         }
     );
+
     return (
         <form>
             <label htmlFor="name">Name</label>
@@ -23,6 +25,7 @@ function Form() {
                 id="job"
                 value={person.job}
                 onChange={handleChange} />
+            <input type="button" value="Submit" onClick={submitForm} />
         </form>
     );
 
@@ -36,6 +39,11 @@ function Form() {
             setPerson(
                 {name: value, job: person['job']}   
             );
+    }
+
+    function submitForm() {
+        props.handleSubmit(person);
+        setPerson({name: '', job: ''});
     }
 }
 
