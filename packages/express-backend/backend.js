8cca75c1361ca68ebb05d4c7ebc19ffe84cabda6
Tyler Baxter <tybaxter@calpoly.edu>
2023-10-02 14:30:30 -0700

    users by id

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -32,12 +32,26 @@ const users = {
     ]
  }
 
- const findUserByName = (name) => { 
+const findUserByName = (name) => { 
     return users['users_list']
         .filter( (user) => user['name'] === name); 
 }
 
+const findUserById = (id) =>
+    users['users_list']
+        .find( (user) => user['id'] === id);
+
 app.use(express.json());
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params['id']; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send('Resource not found.');
+    } else {
+        res.send(result);
+    }
+});
 
 app.get('/users', (req, res) => {
     const name = req.query.name;
@@ -50,6 +64,7 @@ app.get('/users', (req, res) => {
         res.send(users);
     }
 });
+
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
