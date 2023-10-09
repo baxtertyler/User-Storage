3c6507ca02bb08de2f037ad2e54d99a3a0fd25cd
Tyler Baxter <tybaxter@calpoly.edu>
2023-10-09 09:49:00 -0700

    div tests

---
+++ packages/jest-unit-testing/module.test.js
@@ -0,0 +1,36 @@
+import mut from './module.js'; // MUT = Module Under Test
+
+// sum tests
+
+test('Testing sum -- success', () => {
+  const expected = 30;
+  const got = mut.sum(12, 18);
+  expect(got).toBe(expected);
+});
+
+// dev tests
+
+test('Testing dev -- success', () => {
+    const expected = 10;
+    const got = mut.div(20, 2);
+    expect(got).toBe(expected);
+});
+
+test('Testing dev -- success', () => {
+    const expected = 3;
+    const got = mut.div(18, 6);
+    expect(got).toBe(expected);
+});
+
+test('Testing dev -- decimal', () => {
+    const expected = 0.5;
+    const got = mut.div(1, 2);
+    expect(got).toBe(expected);
+});
+
+test('Testing dev -- negative', () => {
+    const expected = -4;
+    const got = mut.div(-8, 2);
+    expect(got).toBe(expected);
+});
+
